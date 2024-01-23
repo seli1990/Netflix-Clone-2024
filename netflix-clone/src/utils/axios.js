c8678f0c7e68329c17bddf525412271e2f9cac06
seli1990 <153053446+seli1990@users.noreply.github.com>
2024-01-22 23:52:48 -0600

    banner

--- netflix-clone/src/utils/axios.js
+++ netflix-clone/src/utils/axios.js
@@ -1,6 +1,8 @@
-import axios from "axios";
-const instance=axios.create({
-   baseURL:"https://api.themoviesdb.org/3" 
-})
 
-export default instance
+import axios from "axios";
+
+const instance = axios.create({
+   baseURL: "https://api.themoviedb.org/3" 
+});
+
+export default instance;
